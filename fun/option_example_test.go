44e8c817d83b85228eaaf866340f9f20dad94089
alekseevi15 <alekseevi15@gmail.com>
2022-09-22 09:11:10 +0200

    Added Option examples: Filter, Fold

---
+++ fun/option_example_test.go
@@ -0,0 +1,101 @@
+package fun
+
+import (
+	"fmt"
+)
+
+func ExampleOption_Map_eg1() {
+	r := Some("route").Map(func(a string) string { return a + "60" })
+	fmt.Println(r)
+	// Output: Some(route60)
+}
+
+func ExampleOption_Map_eg2() {
+	r := None[string]().Map(func(a string) string { return a + "60" })
+	fmt.Println(r)
+	// Output: None
+}
+
+func ExampleMapOption_eg1() {
+	r := MapOption(Some(60), func(a int) string { return "route" + fmt.Sprint(a) })
+	fmt.Println(r)
+	// Output: Some(route60)
+}
+
+func ExampleMapOption_eg2() {
+	r := MapOption(None[int](), func(a int) string { return "route" + fmt.Sprint(a) })
+	fmt.Println(r)
+	// Output: None
+}
+
+func ExampleOption_FlatMap_eg1() {
+	r := Some("route").FlatMap(func(a string) Option[string] { return Some(a + "60") })
+	fmt.Println(r)
+	// Output: Some(route60)
+}
+
+func ExampleOption_FlatMap_eg2() {
+	r := Some("route").FlatMap(func(a string) Option[string] { return None[string]() })
+	fmt.Println(r)
+	// Output: None
+}
+
+func ExampleFlatMapOption_eg1() {
+	r := FlatMapOption(Some(60), func(a int) Option[string] { return Some("route" + fmt.Sprint(a)) })
+	fmt.Println(r)
+	// Output: Some(route60)
+}
+
+func ExampleFlatMapOption_eg2() {
+	r := FlatMapOption(Some(60), func(a int) Option[string] { return None[string]() })
+	fmt.Println(r)
+	// Output: None
+}
+
+func ExampleFlatMapOption_eg3() {
+	r := FlatMapOption(None[int](), func(a int) Option[string] { return Some("route" + fmt.Sprint(a)) })
+	fmt.Println(r)
+	// Output: None
+}
+
+func ExampleOption_Filter_eg1() {
+	r := Some(5).Filter(func(a int) bool { return a < 10 })
+	fmt.Println(r)
+	// Output: Some(10)
+}
+
+func ExampleOption_Filter_eg2() {
+	r := Some(10).Filter(func(a int) bool { return a > 10 })
+	fmt.Println(r)
+	// Output: None
+}
+
+func ExampleOption_Filter_eg3() {
+	r := None[int]().Filter(func(a int) bool { return a < 10 })
+	fmt.Println(r)
+	// Output: None
+}
+
+func ExampleOption_Fold_eg1() {
+	r := Some(5).Fold(1, func(a int) int { return a * 2 })
+	fmt.Println(r)
+	// Output: 10
+}
+
+func ExampleOption_Fold_eg2() {
+	r := None[int]().Fold(1, func(a int) int { return a * 2 })
+	fmt.Println(r)
+	// Output: 1
+}
+
+func ExampleFoldOption_eg1() {
+	r := FoldOption(Some(60), "route0", func(a int) string { return "route" + fmt.Sprint(a) })
+	fmt.Println(r)
+	// Output: route60
+}
+
+func ExampleFoldOption_eg2() {
+	r := FoldOption(None[int](), "route0", func(a int) string { return "route" + fmt.Sprint(a) })
+	fmt.Println(r)
+	// Output: route0
+}

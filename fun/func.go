7ee728175d2d07fc20c05b54a8ce5731c8585844
alekseevi15 <alekseevi15@gmail.com>
2022-11-02 11:52:54 +0100

    Added PartialApply functions + tests

--- fun/func.go
+++ fun/func.go
@@ -1,5 +1,61 @@
 package fun
 
+// Partially applies a 2-argument function `f` to its 1st argument (A). Returns a function of 1 remaining argument (B).
+func Apply2Partial_1[A, B, C any](f func(A, B) C, a A) func(B) C {
+	return func(b B) C {
+		return f(a, b)
+	}
+}
+
+// Partially applies a 2-argument function `f` to its 2nd argument (B). Returns a function of 1 remaining argument (A).
+func Apply2Partial_2[A, B, C any](f func(A, B) C, b B) func(A) C {
+	return func(a A) C {
+		return f(a, b)
+	}
+}
+
+// Partially applies a 3-argument function `f` to its 1st argument (A). Returns a function of 2 remaining arguments (B, C).
+func Apply3Partial_1[A, B, C, D any](f func(A, B, C) D, a A) func(B, C) D {
+	return func(b B, c C) D {
+		return f(a, b, c)
+	}
+}
+
+// Partially applies a 3-argument function `f` to its 2nd argument (B). Returns a function of 2 remaining arguments (A, C).
+func Apply3Partial_2[A, B, C, D any](f func(A, B, C) D, b B) func(A, C) D {
+	return func(a A, c C) D {
+		return f(a, b, c)
+	}
+}
+
+// Partially applies a 3-argument function `f` to its 3rd argument (C). Returns a function of 2 remaining arguments (A, B).
+func Apply3Partial_3[A, B, C, D any](f func(A, B, C) D, c C) func(A, B) D {
+	return func(a A, b B) D {
+		return f(a, b, c)
+	}
+}
+
+// Partially applies a 3-argument function `f` to its 1st & 2nd arguments (A, B). Returns a function of 1 remaining argument (C).
+func Apply3Partial_1_2[A, B, C, D any](f func(A, B, C) D, a A, b B) func(C) D {
+	return func(c C) D {
+		return f(a, b, c)
+	}
+}
+
+// Partially applies a 3-argument function `f` to its 1st & 3rd arguments (A, C). Returns a function of 1 remaining argument (B).
+func Apply3Partial_1_3[A, B, C, D any](f func(A, B, C) D, a A, c C) func(B) D {
+	return func(b B) D {
+		return f(a, b, c)
+	}
+}
+
+// Partially applies a 3-argument function `f` to its 2nd & 3rd arguments (B, C). Returns a function of 1 remaining argument (A).
+func Apply3Partial_2_3[A, B, C, D any](f func(A, B, C) D, b B, c C) func(A) D {
+	return func(a A) D {
+		return f(a, b, c)
+	}
+}
+
 // Takes 2 functions f(A)=>B and g(B)=>C and returns a composed function h(A)=>C.
 func Compose2[A, B, C any](f func(A) B, g func(B) C) func(A) C {
 	return func(a A) C { return g(f(a)) }
@@ -30,20 +86,6 @@ func Curry3[A, B, C, D any](f func(A, B, C) D) func(A) func(B) func(C) D {
 	}
 }
 
-// Transforms a chain of 2 1-argument functions into a function of 2 arguments.
-func UnCurry2[A, B, C any](f func(A) func(B) C) func(A, B) C {
-	return func(a A, b B) C {
-		return f(a)(b)
-	}
-}
-
-// Transforms a chain of 3 1-argument functions into a function of 3 arguments.
-func UnCurry3[A, B, C, D any](f func(A) func(B) func(C) D) func(A, B, C) D {
-	return func(a A, b B, c C) D {
-		return f(a)(b)(c)
-	}
-}
-
 // Transforms a function of 2 arguments into a function of 1 tupled argument.
 func Tupled2[A, B, C any](f func(A, B) C) func(Tuple2[A, B]) C {
 	return func(t Tuple2[A, B]) C {
@@ -55,6 +97,20 @@ func Tupled2[A, B, C any](f func(A, B) C) func(Tuple2[A, B]) C {
 func Tupled3[A, B, C, D any](f func(A, B, C) D) func(Tuple3[A, B, C]) D {
 	return func(t Tuple3[A, B, C]) D {
 		return f(t.a, t.b, t.c)
+	}
+}
+
+// Transforms a chain of 2 1-argument functions into a function of 2 arguments.
+func UnCurry2[A, B, C any](f func(A) func(B) C) func(A, B) C {
+	return func(a A, b B) C {
+		return f(a)(b)
+	}
+}
+
+// Transforms a chain of 3 1-argument functions into a function of 3 arguments.
+func UnCurry3[A, B, C, D any](f func(A) func(B) func(C) D) func(A, B, C) D {
+	return func(a A, b B, c C) D {
+		return f(a)(b)(c)
 	}
 }
 
--- fun/func_test.go
+++ fun/func_test.go
@@ -7,14 +7,100 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestFuncApply2Partial_1(t *testing.T) {
+	//given
+	f := func(a int, b bool) string {
+		return fmt.Sprint(a) + fmt.Sprint(b)
+	}
+	//when
+	p := Apply2Partial_1(f, 10)
+	//then
+	assert.Equal(t, f(10, true), p(true))
+}
+
+func TestFuncApply2Partial_2(t *testing.T) {
+	//given
+	f := func(a int, b bool) string {
+		return fmt.Sprint(a) + fmt.Sprint(b)
+	}
+	//when
+	p := Apply2Partial_2(f, true)
+	//then
+	assert.Equal(t, f(10, true), p(10))
+}
+
+func TestFuncApply3Partial_1(t *testing.T) {
+	//given
+	f := func(a int, b bool, c float64) string {
+		return fmt.Sprint(a) + fmt.Sprint(b) + fmt.Sprint(c)
+	}
+	//when
+	p := Apply3Partial_1(f, 10)
+	//then
+	assert.Equal(t, f(10, true, 5.5), p(true, 5.5))
+}
+
+func TestFuncApply3Partial_2(t *testing.T) {
+	//given
+	f := func(a int, b bool, c float64) string {
+		return fmt.Sprint(a) + fmt.Sprint(b) + fmt.Sprint(c)
+	}
+	//when
+	p := Apply3Partial_2(f, true)
+	//then
+	assert.Equal(t, f(10, true, 5.5), p(10, 5.5))
+}
+
+func TestFuncApply3Partial_3(t *testing.T) {
+	//given
+	f := func(a int, b bool, c float64) string {
+		return fmt.Sprint(a) + fmt.Sprint(b) + fmt.Sprint(c)
+	}
+	//when
+	p := Apply3Partial_3(f, 5.5)
+	//then
+	assert.Equal(t, f(10, true, 5.5), p(10, true))
+}
+
+func TestFuncApply3Partial_1_2(t *testing.T) {
+	//given
+	f := func(a int, b bool, c float64) string {
+		return fmt.Sprint(a) + fmt.Sprint(b) + fmt.Sprint(c)
+	}
+	//when
+	p := Apply3Partial_1_2(f, 10, true)
+	//then
+	assert.Equal(t, f(10, true, 5.5), p(5.5))
+}
+
+func TestFuncApply3Partial_1_3(t *testing.T) {
+	//given
+	f := func(a int, b bool, c float64) string {
+		return fmt.Sprint(a) + fmt.Sprint(b) + fmt.Sprint(c)
+	}
+	//when
+	p := Apply3Partial_1_3(f, 10, 5.5)
+	//then
+	assert.Equal(t, f(10, true, 5.5), p(true))
+}
+
+func TestFuncApply3Partial_2_3(t *testing.T) {
+	//given
+	f := func(a int, b bool, c float64) string {
+		return fmt.Sprint(a) + fmt.Sprint(b) + fmt.Sprint(c)
+	}
+	//when
+	p := Apply3Partial_2_3(f, true, 5.5)
+	//then
+	assert.Equal(t, f(10, true, 5.5), p(10))
+}
+
 func TestFuncCompose2(t *testing.T) {
 	//given
 	f := func(a int) bool { return a != 0 }
 	g := func(b bool) string { return fmt.Sprint(b) }
-
 	//when
 	h := Compose2(f, g)
-
 	//then
 	assert.Equal(t, g(f(1)), h(1))
 }
@@ -24,10 +110,8 @@ func TestFuncCompose3(t *testing.T) {
 	f := func(a int) string { return fmt.Sprint(a) }
 	g := func(b string) bool { return b != "" }
 	h := func(c bool) string { return fmt.Sprint(c) }
-
 	//when
 	j := Compose3(f, g, h)
-
 	//then
 	assert.Equal(t, h(g(f(1))), j(1))
 }
@@ -52,6 +136,28 @@ func TestFuncCurry3(t *testing.T) {
 	curriedF := Curry3(f)
 	//then
 	assert.Equal(t, f(1, true, 5.5), curriedF(1)(true)(5.5))
+}
+
+func TestFuncTupled2(t *testing.T) {
+	//given
+	f := func(a int, b bool) string {
+		return fmt.Sprint(a) + fmt.Sprint(b)
+	}
+	//when
+	tupledF := Tupled2(f)
+	//then
+	assert.Equal(t, f(1, true), tupledF(Tup2(1, true)))
+}
+
+func TestFuncTupled3(t *testing.T) {
+	//given
+	f := func(a int, b bool, c float64) string {
+		return fmt.Sprint(a) + fmt.Sprint(b) + fmt.Sprint(c)
+	}
+	//when
+	tupledF := Tupled3(f)
+	//then
+	assert.Equal(t, f(1, true, 5.5), tupledF(Tup3(1, true, 5.5)))
 }
 
 func TestFuncUnCurry2(t *testing.T) {
@@ -80,4 +186,26 @@ func TestFuncUnCurry3(t *testing.T) {
 	unCurriedF := UnCurry3(f)
 	//then
 	assert.Equal(t, f(1)(true)(5.5), unCurriedF(1, true, 5.5))
+}
+
+func TestFuncUnTupled2(t *testing.T) {
+	//given
+	f := func(t Tuple2[int, bool]) string {
+		return fmt.Sprint(t.a) + fmt.Sprint(t.b)
+	}
+	//when
+	unTupledF := UnTupled2(f)
+	//then
+	assert.Equal(t, f(Tup2(1, true)), unTupledF(1, true))
+}
+
+func TestFuncUnTupled3(t *testing.T) {
+	//given
+	f := func(t Tuple3[int, bool, float64]) string {
+		return fmt.Sprint(t.a) + fmt.Sprint(t.b) + fmt.Sprint(t.c)
+	}
+	//when
+	unTupledF := UnTupled3(f)
+	//then
+	assert.Equal(t, f(Tup3(1, true, 5.5)), unTupledF(1, true, 5.5))
 }
